9a90f0a2356529db7fbd88f90fd1855ec9258421
Sol Zilberman <solzilbermancode@gmail.com>
2020-04-09 00:19:24 -0500

    completed front and backend - deploying to heroku

---
+++ public/precache-manifest.ddb017b082c08735fdc4bda39560a5ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a7c6324e8c8f0b74ba1b3f37bb9a61d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c723bc80997bca2c6235",
+    "url": "/static/css/main.f5bbf061.chunk.css"
+  },
+  {
+    "revision": "0b9b409d29bf88fbe3e7",
+    "url": "/static/js/2.d2873664.chunk.js"
+  },
+  {
+    "revision": "f0d43bb23094b147d2c500466b241ef2",
+    "url": "/static/js/2.d2873664.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c723bc80997bca2c6235",
+    "url": "/static/js/main.291b526e.chunk.js"
+  },
+  {
+    "revision": "702554416a81a1e3a894",
+    "url": "/static/js/runtime-main.784c586e.js"
+  }
+]);
--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const graphqlHTTP = require("express-graphql");
 const schema = require("./schema");
-const app = express();
+const cors = require('cors');
+const path = require('path');
 
+const app = express();
+//allow cross-origin
+app.use(cors());
 app.use(
   "/graphql",
   graphqlHTTP({
@@ -10,7 +14,13 @@ app.use(
     graphiql: true,
   })
 );
-const PORT = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+})
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}...`)
 })
